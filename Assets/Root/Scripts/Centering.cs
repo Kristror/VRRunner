0aab010c4b4d5b0cc40f24c6e618c9c47172756d
Kristror <daniil.bezy@ya.ru>
2022-12-31 12:56:01 +0300

    HTC homework

---
+++ Assets/Root/Scripts/Centering.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Runner
+{
+    public class Centering : MonoBehaviour
+    {
+        [SerializeField] private Transform _pivot;
+        [SerializeField] private CapsuleCollider _collider;
+
+        private Vector3 vec;
+
+        private void OnValidate()
+        {
+            _collider = GetComponent<CapsuleCollider>();
+        }
+
+        void Start()
+        {
+            FindTeleportPivotAndTarget();
+            vec.y = _collider.center.y;
+        }
+
+        void Update()
+        {
+            vec.x = _pivot.localPosition.x;
+            vec.z = _pivot.localPosition.z;
+        }
+
+        private void FindTeleportPivotAndTarget() 
+        {
+            foreach(var cam in Camera.allCameras)
+            {
+                if(!cam.enabled) { continue; }
+                if(cam.stereoTargetEye != StereoTargetEyeMask.Both) { continue; }
+                _pivot = cam.transform;
+            }
+        }
+    }
+}
---
+++ Assets/Root/Scripts/MyTeleport.cs
@@ -0,0 +1,33 @@
+﻿using HTC.UnityPlugin.Vive;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Runner
+{
+    public class MyTeleport : Teleportable
+    {
+        [SerializeField] private float _speed;
+        [SerializeField] private float _cooldown;
+
+        public override IEnumerator StartTeleport(RaycastResult hitResult, Vector3 position, Quaternion rotation, float delay)
+        {
+            while (true)
+            {           
+                target.position = Vector3.MoveTowards(target.position, position, _speed * Time.deltaTime);
+
+                Vector3 v = position;
+                v.y = target.position.y;
+
+                if(Vector3.Distance(target.position, v) < 0.1f)
+                {
+                    yield return new WaitForSeconds(_cooldown);
+                    teleportCoroutine = null;
+                    yield break;
+                }
+
+                yield return new WaitForFixedUpdate();
+            }
+        }
+    }
+}
