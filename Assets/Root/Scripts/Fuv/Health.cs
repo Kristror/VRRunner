af62f3182f4387e375f0617424caa607277539ea
Kristror <daniil.bezy@ya.ru>
2023-01-18 15:12:44 +0300

    HW5

---
+++ Assets/Root/Scripts/Fuv/Health.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] int maxValue = 10;
+
+    private int value;
+    public int curHealth { get { return value; } }
+
+    private void Start()
+    {
+        value = maxValue;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        if(value > 0)
+        {
+            value -= damage;
+            if (value < 0) value = 0;
+        }
+    }
+}
+        
